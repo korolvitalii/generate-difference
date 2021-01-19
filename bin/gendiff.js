c882a9b8f0b34fe68298d203fefd4cfabf3418e5
Vitalii Korol <korolvitalik@gmail.com>
2021-01-19 23:04:52 +0100

    add new option in gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,7 @@ import program from 'commander';
 program
   .description('Compares two configuration files and s hows a difference.')
   .option('-V, --version', 'output the version number')
-  .helpOption('-h, --HELP', 'read more information');
+  .helpOption('-h, --HELP', 'read more information')
+  .option('-f, --format [type]', 'output format')
 
 program.parse(process.argv);
