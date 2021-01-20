0930db790f5245d8053218629d9e02e4bc36aec7
Vitalii Korol <korolvitalik@gmail.com>
2021-01-20 12:18:10 +0100

    working with diff function

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 
 import program from 'commander';
+import genDiff from '../index.js';
 
 program
   .description('Compares two configuration files and s hows a difference.')
   .option('-V, --version', 'output the version number')
   .helpOption('-h, --HELP', 'read more information')
   .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2, program.format)));
 
 program.parse(process.argv);
